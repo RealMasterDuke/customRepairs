7b4299daa0545dc6b252cabbd4899297be71df6b
RealMasterDuke <christian.berg@io-solution.de>
2014-09-14 13:37:57 +0200

    Merge branch 'master' of https://github.com/RealMasterDuke/customRepairs
    Conflicts:
    	.idea/workspace.xml

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -12,7 +12,7 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
     @products = @category.products.all
-    @products= @category.products.build
+    # @products= @category.products.build
   end
 
   # GET /categories/new
