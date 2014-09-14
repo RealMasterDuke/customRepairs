daf379014ba24e0f2d62e183a5f122da79fa863e
Arthur <liebhardt.arthur@gmail.de>
2014-09-14 22:17:13 +0200

    1 failure beseitigt

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140914122415) do
+ActiveRecord::Schema.define(version: 20140914131214) do
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -60,7 +60,7 @@ ActiveRecord::Schema.define(version: 20140914122415) do
 
   create_table "line_items", force: true do |t|
     t.decimal  "unit_price"
-    t.decimal  "product_id"
+    t.integer  "product_id"
     t.integer  "cart_id"
     t.integer  "quantity"
     t.datetime "created_at"
